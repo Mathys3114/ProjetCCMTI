4d291bb88a8d973b034b02ef3b9343ffbb80f314
3nd3rh3o <theenderman3333@gmail.com>
2026-02-18 20:00:15 +0100

    fini, manque plus qu'à écrire un prog pour mettre d'une autre couleur les pixels sur les bords d'un super pixel

--- code/src/SLIC.cpp
+++ code/src/SLIC.cpp
@@ -3,25 +3,43 @@
 #include <vector>
 #include "ImageBase.h"
 #include "slic.hpp"
-const char * USAGE = "SLIC K imgIN.ppm";
+const char * USAGE = "SLIC K m g imgIN.ppm imgOUT.ppm";
 
 
 int main(int argc, char * argv[])
 {
-    if (argc != 3)
+    if (argc != 6)
     {
         std::cerr << USAGE << std::endl;
         return 1;
     }
     double K = std::atof(argv[1]); // number of superpixel
-    char * imINn = argv[2];
+    double m = std::atof(argv[2]); // scaling spatial/color
+    double g = std::atof(argv[3]); // search window
+    char * imINn = argv[4];
+    char * imOUTn = argv[5];
     ImageBase imIN;
     imIN.load(imINn);
 
     double w = imIN.getWidth();
     double h = imIN.getHeight();
 
-    std::vector<Superpixel> grid = genGrid(w, h, K);
+    double N = w * h;
+    double S = std::sqrt(N / K); // step size
 
+    std::vector<Superpixel> grid = genGrid(w, h, K);
+    grid = nudgeAlongGradient(imIN, grid, g);
+    std::vector<int> l = slic(imIN, S, grid, m);
+
+
+    ImageBase imOUT(imIN.getWidth(), imIN.getHeight(), true);
+
+    for (size_t i = 0; i < l.size(); i++)
+    {
+        imOUT.getData()[i*3] = grid[l[i]].mr;
+        imOUT.getData()[i*3+1] = grid[l[i]].mg;
+        imOUT.getData()[i*3+2] = grid[l[i]].mb;
+    }
+    imOUT.save(imOUTn);
     return 0;
 }
--- libs/slic.hpp
+++ libs/slic.hpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <vector>
+#include <map>
 #include "ImageBase.h"
 
 class Superpixel
@@ -58,17 +59,19 @@ std::vector<Superpixel> & nudgeAlongGradient(ImageBase & imIN, std::vector<Super
         for (int h = -g; h < g;h++)
             for (int w = -g; w < g; w++)
             {
-                double r0 = imIN[(y+h)*3][(x+w)*3];
-                double g0 = imIN[(y+h)*3][(x+w)*3+1];
-                double b0 = imIN[(y+h)*3][(x+w)*3+2];
+                size_t dy = std::max(0, std::min(imIN.getHeight() - 2, y+h));
+                size_t dx = std::max(0, std::min(imIN.getWidth() - 2, x+w));
+                double r0 = imIN[(dy)*3][(dx)*3];
+                double g0 = imIN[(dy)*3][(dx)*3+1];
+                double b0 = imIN[(dy)*3][(dx)*3+2];
                 
-                double r1 = imIN[(y+h)*3][(x+w+1)*3];
-                double g1 = imIN[(y+h)*3][(x+w+1)*3+1];
-                double b1 = imIN[(y+h)*3][(x+w+1)*3+2];
+                double r1 = imIN[(dy)*3][(dx+1)*3];
+                double g1 = imIN[(dy)*3][(dx+1)*3+1];
+                double b1 = imIN[(dy)*3][(dx+1)*3+2];
 
-                double r2 = imIN[(y+1+h)*3][(x+w)*3];
-                double g2 = imIN[(y+1+h)*3][(x+w)*3+1];
-                double b2 = imIN[(y+1+h)*3][(x+w)*3+2];
+                double r2 = imIN[(dy+1)*3][(dx)*3];
+                double g2 = imIN[(dy+1)*3][(dx)*3+1];
+                double b2 = imIN[(dy+1)*3][(dx)*3+2];
 
                 double r0s = r0 - r1;
                 double g0s = g0 - g1;
@@ -88,4 +91,122 @@ std::vector<Superpixel> & nudgeAlongGradient(ImageBase & imIN, std::vector<Super
             }
     }
     return grid;
+}
+double _ds(
+    double px, double py, 
+    double sx, double sy
+)
+{
+    double x = (px - sx);
+    double y = (py - sy);
+    return std::sqrt((x * x) + (y * y));
+}
+
+double _dc(
+    double pr, double pg, double pb,
+    double sr, double sg, double sb
+)
+{
+    double r = (pr - sr);
+    double g = (pg - sg);
+    double b = (pb - sb);
+    return std::sqrt((r*r)+(g*g)+(b*b));
+}
+
+double dist(
+    double px, double py, double pr, double pg, double pb, 
+    double sx, double sy, double sr, double sg, double sb, 
+    double m, double S
+)
+{
+    double ds = _ds(px, py, sx, sy);
+    double dc = _dc(pr, pg, pb, sr, sg, sb);
+    return m*m*((ds * ds) / (S * S)) + (dc * dc);
+}
+
+std::vector<Superpixel> _updt(ImageBase & imIN, std::vector<int> & l, size_t K)
+{
+    std::vector<Superpixel> nG(K);
+    std::vector<double> n(K);
+    for (size_t k = 0; k < K; k++)
+    {
+        nG[k] = Superpixel(0, 0, 0, 0, 0);
+        n[k] = 0.0;
+    }
+
+    const int W = imIN.getWidth();
+    const int H = imIN.getHeight();
+    const int size = W * H;
+
+    for (int i = 0; i < size; i++)
+    {
+        const int k = l[i];
+        const int x = i % W;
+        const int y = i / W;
+
+        nG[k].mx += (double)x;
+        nG[k].my += (double)y;
+        nG[k].mr += (double)imIN.getData()[i * 3];
+        nG[k].mg += (double)imIN.getData()[i * 3 + 1];
+        nG[k].mb += (double)imIN.getData()[i * 3 + 2];
+        n[k] += 1.0;
+    }
+    for (size_t k = 0; k < K; k++)
+    {
+        if (n[k] <= 0.0)
+            continue;
+        nG[k].mx /= n[k];
+        nG[k].my /= n[k];
+        nG[k].mr /= n[k];
+        nG[k].mg /= n[k];
+        nG[k].mb /= n[k];
+    }
+    return nG;
+}
+
+std::vector<int> slic(ImageBase & imIN, int S, std::vector<Superpixel> & grid, double m)
+{
+    std::vector<double> d(imIN.getWidth() * imIN.getHeight());
+    std::vector<int> l(imIN.getWidth() * imIN.getHeight());
+
+    for (size_t k = 0; k < grid.size(); k++)
+    {
+        int x = std::max(0, std::min((int)grid[k].mx, imIN.getWidth() - 1));
+        int y = std::max(0, std::min((int)grid[k].my, imIN.getHeight() - 1));
+        grid[k].mr = (double)imIN[y * 3][x * 3];
+        grid[k].mg = (double)imIN[y * 3][x * 3 + 1];
+        grid[k].mb = (double)imIN[y * 3][x * 3 + 2];
+    }
+
+    double E = MAXFLOAT;
+    while (E > 0.5)
+    {
+        for (size_t i = 0; i < d.size(); i++)
+            d[i] = MAXFLOAT;
+        for (size_t k = 0; k < grid.size(); k++)
+            for (int h = -S; h <= S; h++)
+                for (int w = -S; w <= S; w++)
+                {
+                    int x = std::max(0, std::min(((int)grid[k].mx + w), imIN.getWidth() - 1));
+                    int y = std::max(0, std::min(((int)grid[k].my + h), imIN.getHeight() - 1));
+                    double e = dist(x, y, imIN[y*3][x*3], imIN[y*3][x*3+1], imIN[y*3][x*3+2], grid[k].mx, grid[k].my, grid[k].mr, grid[k].mg, grid[k].mb, m, S);
+                    if (d[y * imIN.getWidth() + x] > e)
+                    {
+                        d[y * imIN.getWidth() + x] = e;
+                        l[y * imIN.getWidth() + x] = k;
+                    }
+                }
+        std::vector<Superpixel> nG = _updt(imIN, l, grid.size());
+        double acc = 0.0;
+        for (size_t k = 0; k < grid.size(); k++)
+        {
+            double ds = _ds(grid[k].mx, grid[k].my, nG[k].mx, nG[k].my);
+            acc += (ds * ds);
+        }
+        acc/=grid.size();
+        E = acc;
+        grid = nG;
+    }
+
+    return l;
 }
